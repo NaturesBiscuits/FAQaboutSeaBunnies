12f784c2ec100c66d8b2ae6e54c1ed18a911303d
NaturesBiscuits <johnvincentflores2424@gmail.com>
2024-07-04 22:27:54 +0800

    switching to forEach method

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 let box1 = document.getElementById('box1');
 let box2 = document.getElementById('box2');
 let box3 = document.getElementById('box3');
+let boxes = document.querySelectorAll('.box');
 
 
 
-box1.addEventListener('click', () => {
-    console.log("box1"); // for debuging
-});
-
-box2.addEventListener('click', () => {
-    console.log("box2"); // for debuging
-});
-
-box3.addEventListener('click', () => {
-    console.log("box3"); // for debuging
+boxes.forEach(box => {
+     box.addEventListener('click', () => {
+        let text;
+        switch(box.id){
+            case 'box1':
+                text = 'question 1'
+                break;
+            case 'box2':
+                text = 'question 2'
+                break;
+            case 'box3':
+                text = 'question 3'
+                break;
+        }
+        console.log(text);
+    });
 });
