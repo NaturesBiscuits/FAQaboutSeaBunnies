5bdbd86dd22fd540f993c3174b719f3f67bebbf8
NaturesBiscuits <johnvincentflores2424@gmail.com>
2024-07-04 21:46:53 +0800

    few changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let box1 = document.getElementById('box1');
-let box2 = document.getElementById('box2');
-let box3 = document.getElementById('box3');
+let box01 = document.getElementById('box1');
+let box02 = document.getElementById('box2');
+let box03 = document.getElementById('box3');
 
-box1.addEventListener('click', () => {
+box01.addEventListener('click', () => {
     console.log("hey?");
 });
