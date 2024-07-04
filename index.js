3e5f7b1db2be6ac14a32111fec1380fc1479a071
NaturesBiscuits <johnvincentflores2424@gmail.com>
2024-07-04 22:03:25 +0800

    added click function in questions

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let box01 = document.getElementById('box1');
-let box02 = document.getElementById('box2');
-let box03 = document.getElementById('box3');
+let box1 = document.getElementById('box1');
+let box2 = document.getElementById('box2');
+let box3 = document.getElementById('box3');
 
-box01.addEventListener('click', () => {
-    console.log("hey?");
+
+
+box1.addEventListener('click', () => {
+    console.log("box1"); // for debuging
+});
+
+box2.addEventListener('click', () => {
+    console.log("box2"); // for debuging
+});
+
+box3.addEventListener('click', () => {
+    console.log("box3"); // for debuging
 });
