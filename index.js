c9c69b1faf9e32bff6e6ddfbf42b10936293eb94
NaturesBiscuits <johnvincentflores2424@gmail.com>
2024-07-04 21:36:02 +0800

    added  css and js

---
+++ index.js
@@ -0,0 +1,7 @@
+let box1 = document.getElementById('box1');
+let box2 = document.getElementById('box2');
+let box3 = document.getElementById('box3');
+
+box1.addEventListener('click', () => {
+    
+});
